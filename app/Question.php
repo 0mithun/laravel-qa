3279ad2f22809f735c491de732dd226fe9935de7
Mithun Halder <mithunrptc@gmail.com>
2019-02-09 18:11:11 +0600

    Implement show functionality of the question

--- app/Question.php
+++ app/Question.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Parsedown;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
@@ -25,7 +26,7 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        return route('questions.show', $this->id);
+        return route('questions.show', $this->slug);
     }
 
     public function getCreatedDateAttribute()
@@ -42,6 +43,11 @@ class Question extends Model
             return 'answered';
         }
         return 'unanswered';
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return Parsedown::instance()->text($this->body);
     }
 }
 
