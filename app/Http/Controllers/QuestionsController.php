7a289702bbcdbcbb412fe5dd651169a1eab7ee17
Mithun Halder <mithunrptc@gmail.com>
2019-02-09 19:16:02 +0600

    Implement authorization using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,10 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',['except'=>['index','show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -61,11 +65,8 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        if(\Gate::denies('update-question', $question)){
-            \abort(403,'Access Denied');
-        }
+        $this->authorize('update', $question);
         return view('questions.edit',compact('question'));
-        
     }
 
     /**
@@ -77,10 +78,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
-        if(\Gate::denies('update-question', $question)){
-            \abort(403,'Access Denied');
-        }
-
+        $this->authorize('update', $question);
         $question->update($request->only('title','body'));
         return redirect()->route('questions.index')->with('success','Your question has been updated.');
     }
@@ -93,9 +91,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        if(\Gate::denies('delete-question', $question)){
-            \abort(403,'Access Denied');
-        }
+        $this->authorize('delete', $question);
         $question->delete();
         return redirect()->route('questions.index')->with('success','Your question has been deleted');
     }
