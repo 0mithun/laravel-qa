7c0e5ceaf23479905b0f11b3e538e3fe3b9669b2
Mithun Halder <mithunrptc@gmail.com>
2019-02-21 21:34:15 +0600

    Implement load more answers feature

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -31,7 +31,8 @@
             </div>
         </div>
     </div>
-    <answers :answers="{{ $question->answers }}" :count="{{  $question->answers_count }}"></answers>
+    <answers :question="{{ $question }}"></answers>
+    {{-- <answers :answers="{{ $question->answers }}" :count="{{  $question->answers_count }}"></answers> --}}
     @include ('answers._create')
 </div>
 @endsection
